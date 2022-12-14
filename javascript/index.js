c2cf0137b476c5b98009cbe3b4a7770c0716f989
Kirito-san-88 <vincentbsr88@gmail.com>
2022-12-14 10:52:07 +0100

    Changing  media query for smarphone

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const section = document.querySelector("section");
   const playerLivesCount = document.querySelector("span");
-  let playerLives = 10;
+  let playerLives = 20;
   const modal = document.querySelector(".modal");
   const span = document.querySelector(".close");
   let paragrapheText = document.querySelector(".winOrLose");
@@ -199,7 +199,7 @@ document.addEventListener("DOMContentLoaded", () => {
         section.style.pointerEvents = "all";
       }, 1000);
     });
-    playerLives = 10;
+    playerLives = 20;
     playerLivesCount.textContent = playerLives;
   };
 
