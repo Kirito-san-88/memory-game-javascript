8fc5bc3cee375fe9ec9280976ad14ea2c3dc69ea
Kirito-san-88 <vincentbsr88@gmail.com>
2023-01-30 11:24:06 +0100

    added meta in head

--- javascript/index.js
+++ javascript/index.js
@@ -1,207 +1,312 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const section = document.querySelector("section");
-  const playerLivesCount = document.querySelector("span");
-  let playerLives = 15;
-  const modal = document.querySelector(".modal");
-  const span = document.querySelector(".close");
-  let paragrapheText = document.querySelector(".winOrLose");
+const cardsArray = [
+  {
+    name: "abra",
+    imageSrc: "./img/abra.png",
+  },
+  {
+    name: "bulbizar",
+    imageSrc: "./img/bulbizar.png",
+  },
+  {
+    name: "carapuce",
+    imageSrc: "./img/carapuce.png",
+  },
+  {
+    name: "evoli",
+    imageSrc: "./img/evoli.png",
+  },
+  {
+    name: "mini-draco",
+    imageSrc: "./img/mini-draco.png",
+  },
+  {
+    name: "pikachu",
+    imageSrc: "./img/pikachu.png",
+  },
+  {
+    name: "psykokwak",
+    imageSrc: "./img/psykokwak.png",
+  },
+  {
+    name: "rondoudou",
+    imageSrc: "./img/rondoudou.png",
+  },
+  {
+    name: "ronflex",
+    imageSrc: "./img/ronflex.png",
+  },
+  {
+    name: "salameche",
+    imageSrc: "./img/salameche.png",
+  },
+];
 
-  // link player lives text
-  playerLivesCount.textContent = playerLives;
+const getNumberOfLivesForDifficulty = (difficulty) => {
+  let lives = 0;
+  switch (difficulty) {
+    case "easy":
+      lives = 3;
+      break;
+    case "normal":
+      lives = 5;
+      break;
+    case "hard":
+      lives = 10;
+      break;
+  }
+  return lives;
+};
+// numberOfCards is the number of cards we want to generate in our generateRandomlySortedCardArray(3) method here is 3 unique cards
+const generateRandomlySortedCardArray = (numberOfCards) => {
+  const cardData = cardsArray;
+  cardData.sort(() => Math.random() - 0.5);
+  let selectedCards = cardData.slice(0, numberOfCards);
+  selectedCards = selectedCards.concat(selectedCards);
+  selectedCards.sort(() => Math.random() - 0.5);
+  return selectedCards;
+};
 
-  // we make a const for all the cards in array
-  const cardsArray = () => [
-    {
-      name: "abra",
-      imageSrc: "./img/abra.png",
-    },
-    {
-      name: "abra",
-      imageSrc: "./img/abra.png",
-    },
-    {
-      name: "bulbizar",
-      imageSrc: "./img/bulbizar.png",
-    },
-    {
-      name: "bulbizar",
-      imageSrc: "./img/bulbizar.png",
-    },
-    {
-      name: "carapuce",
-      imageSrc: "./img/carapuce.png",
-    },
-    {
-      name: "carapuce",
-      imageSrc: "./img/carapuce.png",
-    },
-    {
-      name: "evoli",
-      imageSrc: "./img/evoli.png",
-    },
-    {
-      name: "evoli",
-      imageSrc: "./img/evoli.png",
-    },
-    {
-      name: "mini-draco",
-      imageSrc: "./img/mini-draco.png",
-    },
-    {
-      name: "mini-draco",
-      imageSrc: "./img/mini-draco.png",
-    },
-    {
-      name: "pikachu",
-      imageSrc: "./img/pikachu.png",
-    },
-    {
-      name: "pikachu",
-      imageSrc: "./img/pikachu.png",
-    },
-    {
-      name: "psykokwak",
-      imageSrc: "./img/psykokwak.png",
-    },
-    {
-      name: "psykokwak",
-      imageSrc: "./img/psykokwak.png",
-    },
-    {
-      name: "rondoudou",
-      imageSrc: "./img/rondoudou.png",
-    },
-    {
-      name: "rondoudou",
-      imageSrc: "./img/rondoudou.png",
-    },
-    {
-      name: "ronflex",
-      imageSrc: "./img/ronflex.png",
-    },
-    {
-      name: "ronflex",
-      imageSrc: "./img/ronflex.png",
-    },
-    {
-      name: "salameche",
-      imageSrc: "./img/salameche.png",
-    },
-    {
-      name: "salameche",
-      imageSrc: "./img/salameche.png",
-    },
-  ];
+const generateNewCardArrayState = (difficulty) => {
+  // TODO: Ajuster le nombre de cartes selon la difficulté ?
+  let cards;
+  switch (difficulty) {
+    case "easy":
+      cards = generateRandomlySortedCardArray(3);
+      break;
+    case "normal":
+      cards = generateRandomlySortedCardArray(5);
+      break;
+    case "hard":
+      cards = generateRandomlySortedCardArray(10);
+      break;
+  }
 
-  // we have to randomize the cards
-  const random = () => {
-    const cardData = cardsArray();
-    cardData.sort(() => Math.random() - 0.5);
-    return cardData;
-  };
-  // we have to generate the cards
-  const cardGenerate = () => {
-    const cardData = random();
-    // generate HTML
+  return cards.map((card) => new CardState(card.name, card.imageSrc));
+};
 
-    cardData.forEach((item, index) => {
-      const card = document.createElement("div");
-      const face = document.createElement("img");
-      const back = document.createElement("div");
-      card.classList = "card";
-      face.classList = "face";
-      back.classList = "back";
+class CardState {
+  constructor(name, imageSrc, isFlipped = false, isConfirmed = false) {
+    this.name = name;
+    this.imageSrc = imageSrc;
+    this.isFlipped = isFlipped;
+    this.isConfirmed = isConfirmed;
+  }
+
+  changeFlippedFlag(flag) {
+    return new CardState(this.name, this.imageSrc, flag, this.isConfirmed);
+  }
+
+  changeConfirmedFlag(flag) {
+    return new CardState(this.name, this.imageSrc, this.isFlipped, flag);
+  }
+}
+
+class MemoryGameState {
+  constructor(
+    difficulty = "normal",
+    lives = -1,
+    cardArrayState = [],
+    flippedCardIndex = -1
+  ) {
+    this.difficulty = difficulty;
+    this.lives =
+      lives == -1 ? getNumberOfLivesForDifficulty(difficulty) : lives;
+    this.cardArrayState =
+      cardArrayState.length !== 0
+        ? [...cardArrayState]
+        : generateNewCardArrayState(difficulty);
+    this.flippedCardIndex = flippedCardIndex;
+  }
+
+  changeDifficulty(difficulty) {
+    return new MemoryGameState(difficulty);
+  }
+
+  writeCardsToScreen() {
+    const remainingLivesSpan = document.querySelector(".playerLivesCount");
+    remainingLivesSpan.textContent = this.lives.toString();
+    const gameDiv = document.getElementById("game");
+    gameDiv.innerHTML = "";
+    this.cardArrayState.forEach((card, index) => {
+      const cardDiv = document.createElement("div");
+      const cardFaceImg = document.createElement("img");
+      const cardBackDiv = document.createElement("div");
+
+      cardDiv.classList = "card";
+      setTimeout(
+        () =>
+          cardDiv.classList.toggle(
+            "toggleCard",
+            card.isFlipped || card.isConfirmed
+          ),
+        100
+      );
+      cardFaceImg.classList = "face";
+      cardBackDiv.classList = "back";
       // attach the src to the cards
-      face.src = item.imageSrc;
+      cardFaceImg.src = card.imageSrc;
       // attach the name to the cards
-      card.setAttribute("name", item.name);
+      cardDiv.setAttribute("name", card.name);
 
-      // put the card in the div element
-      section.appendChild(card);
-      card.appendChild(face);
-      card.appendChild(back);
+      cardDiv.appendChild(cardFaceImg);
+      cardDiv.appendChild(cardBackDiv);
+      cardDiv.dataset.cardId = index.toString();
 
       // make event click for cards
-      card.addEventListener("click", (event) => {
-        card.classList.toggle("toggleCard");
-        checkCards(event);
-      });
-    });
-  };
-
-  // checks the cards
-
-  const checkCards = (event) => {
-    console.log(event);
-    const clickedCard = event.target;
-    console.log(clickedCard);
-    clickedCard.classList.add("flipped");
-    const flippedCards = document.querySelectorAll(".flipped");
-    const toggleCard = document.querySelectorAll(".toggleCard");
-
-    // checking if the 1st flipped cards is same than the 2nd one
-    if (flippedCards.length === 2) {
-      if (
-        flippedCards[0].getAttribute("name") ===
-        flippedCards[1].getAttribute("name")
-      ) {
-        flippedCards.forEach((card) => {
-          card.classList.remove("flipped");
-          card.style.pointerEvents = "none";
-        });
-      } else {
-        flippedCards.forEach((card) => {
-          card.classList.remove("flipped");
-          // setting Timeout before the card flip back
-          setTimeout(() => card.classList.remove("toggleCard"), 1000);
-        });
-        playerLives--;
-        playerLivesCount.textContent = playerLives;
-        if (playerLives === 0) {
-          modal.style.display = "block";
-          modal.style.backgroundColor = "red";
-          paragrapheText.textContent = "You lose ! \r\n 👎";
-          section.style.pointerEvents = "none";
-          span.addEventListener("click", () => {
-            modal.style.display = "none";
-
-            restart();
-          });
+      cardDiv.addEventListener("click", (event) => {
+        // TODO: Mettre à jour selon l'état cardState
+        if (!card.isConfirmed && !card.isFlipped) {
+          cardDiv.classList.toggle("toggleCard");
+          memGameState = this.checkCards(event);
+          memGameState.writeCardsToScreen();
         }
+      });
+
+      gameDiv.appendChild(cardDiv);
+    });
+  }
+
+  checkCards(event) {
+    let newState = this;
+
+    if (this.flippedCardIndex == -1) {
+      newState = this.updateForSingleCardFlip(event);
+    } else {
+      const clickedCardId = event.target.dataset.cardId;
+      if (this.flippedCardIndex !== clickedCardId) {
+        newState = this.updateForDoubleCardFlip(event);
       }
     }
-    if (toggleCard.length === 20) {
-      modal.style.display = "block";
-      modal.style.backgroundColor = "green";
-      paragrapheText.textContent = "You won! \r\n 👍";
-      section.style.pointerEvents = "none";
-      span.addEventListener("click", () => {
-        modal.style.display = "none";
+    return newState;
+  }
 
-        restart();
-      });
+  updateForSingleCardFlip(event) {
+    const clickedCard = event.target;
+    this.cardArrayState[clickedCard.dataset.cardId].isFlipped = true;
+    this.flippedCardIndex = clickedCard.dataset.cardId;
+
+    return new MemoryGameState(
+      this.difficulty,
+      this.lives,
+      this.cardArrayState,
+      this.flippedCardIndex
+    );
+  }
+
+  updateForDoubleCardFlip(event) {
+    const clickedCard = event.target.dataset.cardId;
+    const [lCard, rCard] = [
+      this.cardArrayState[this.flippedCardIndex],
+      this.cardArrayState[clickedCard],
+    ];
+    lCard.isFlipped = false;
+    rCard.isFlipped = false;
+    if (lCard.name === rCard.name) {
+      lCard.isConfirmed = true;
+      rCard.isConfirmed = true;
+      this.cardArrayState;
+    } else {
+      // setTimeout(() => event.target.classList.remove("toggleCard"), 1200);
+      console.log(event.target);
+      this.lives--;
     }
-  };
-  // making a restart function
-  const restart = () => {
-    let cardData = random();
-    let faces = document.querySelectorAll(".face");
-    let cards = document.querySelectorAll(".card");
-    section.style.pointerEvents = "none";
-    cardData.forEach((item, index) => {
-      cards[index].classList.remove("toggleCard");
-      // need to randomize the cards again
-      // and make a Timeout to not see the img before make the randomize
-      setTimeout(() => {
-        cards[index].style.pointerEvents = "all";
-        faces[index].src = item.imageSrc;
-        cards[index].setAttribute("name", item.name);
-        section.style.pointerEvents = "all";
-      }, 1000);
-    });
-    playerLives = 15;
-    playerLivesCount.textContent = playerLives;
-  };
+    return new MemoryGameState(
+      this.difficulty,
+      this.lives,
+      this.cardArrayState,
+      -1
+    );
+  }
 
-  cardGenerate();
+  // updateForDoubleCardFlip(event) {
+  //   const clickedCard = event.target.dataset.cardId;
+  //   const [lCard, rCard] = [
+  //     this.cardArrayState[this.flippedCardIndex],
+  //     this.cardArrayState[clickedCard],
+  //   ];
+  //   lCard.isFlipped = false;
+  //   rCard.isFlipped = false;
+  //   if (lCard.name === rCard.name) {
+  //     lCard.isConfirmed = true;
+  //     rCard.isConfirmed = true;
+  //   } else {
+  //     this.lives--;
+  //     console.log(this.lives);
+  //   }
+  //   return new MemoryGameState(
+  //     this.difficulty,
+  //     this.lives,
+  //     this.cardArrayState,
+  //     -1
+  //   );
+  // }
+
+  checkIfGameHasEnded() {
+    return (
+      this.lives === 0 || this.cardArrayState.every((card) => card.isConfirmed)
+    );
+  }
+
+  showModalWithLossState() {
+    this.showModal("red", "You lose ! \r\n 👎");
+  }
+
+  showModalWithWinState() {
+    this.showModal("green", "You won! \r\n 👍");
+  }
+
+  showModal(modalColor, textContent) {
+    const modal = document.querySelector(".modal");
+    const modalText = document.querySelector(".winOrLose");
+    const closeModal = document.querySelector(".close");
+    const gameDiv = document.getElementById("game");
+    modal.style.backgroundColor = modalColor;
+    modalText.textContent = textContent;
+    gameDiv.style.pointerEvents = "none";
+    closeModal.addEventListener("click", () => {
+      this.hideModal();
+      memGameState = this.restartGame();
+    });
+  }
+
+  hideModal() {
+    const modal = document.querySelector(".modal");
+    modal.style.display = "none";
+  }
+
+  restartGame() {
+    return new MemoryGameState(this.difficulty);
+  }
+}
+
+let memGameState = new MemoryGameState();
+let showDifficultyOnScreen = document.querySelector(".difficulty");
+const sectionGame = document.getElementById("game");
+document.getElementById("easy").addEventListener("click", () => {
+  memGameState = memGameState.changeDifficulty("easy");
+  showDifficultyOnScreen.textContent =
+    "Vous avez choisi la difficulté : " + memGameState.difficulty;
+  sectionGame.style.gridTemplateColumns = "repeat(3, 8rem)";
+  sectionGame.style.gridTemplateRows = "repeat(2, 8rem)";
+  memGameState.writeCardsToScreen();
 });
+
+document.getElementById("normal").addEventListener("click", () => {
+  memGameState = memGameState.changeDifficulty("normal");
+  showDifficultyOnScreen.textContent =
+    "Vous avez choisi la difficulté : " + memGameState.difficulty;
+  sectionGame.style.gridTemplateColumns = "repeat(5, 8rem)";
+  sectionGame.style.gridTemplateRows = "repeat(2, 8rem)";
+  memGameState.writeCardsToScreen();
+});
+
+document.getElementById("hard").addEventListener("click", () => {
+  memGameState = memGameState.changeDifficulty("hard");
+  showDifficultyOnScreen.textContent =
+    "Vous avez choisi la difficulté : " + memGameState.difficulty;
+  sectionGame.style.gridTemplateColumns = "repeat(5, 8rem)";
+  sectionGame.style.gridTemplateRows = "repeat(5, 8rem)";
+  memGameState.writeCardsToScreen();
+});
+
+memGameState.writeCardsToScreen();
